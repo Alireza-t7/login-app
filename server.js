a55b306cc0b13aed77ab8181136299e26d4eb8a1
Alireza <68426894+Alireza-t7@users.noreply.github.com>
2026-04-03 10:36:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Server is running");
+});
+
+app.post("/login", function (req, res) {
+  const { identifier, password } = req.body;
+
+  console.log("Identifier:", identifier);
+  console.log("Password:", password);
+
+  res.send("Login route working");
+});
+
+app.listen(3000, function () {
+  console.log("Server running on http://localhost:3000");
+});
