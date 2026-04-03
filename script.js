36a6223903ce8be1fe01ccbfb5ced6d5caa6ce3d
Alireza <68426894+Alireza-t7@users.noreply.github.com>
2026-04-03 10:50:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function loginForm() {
     const data = { identifier, password };
 
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://login-app-9ceq.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
