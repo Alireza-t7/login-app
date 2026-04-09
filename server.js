ea4485ec8e3af9ecd133ba9b24f4db5f41ca5823
Alireza <68426894+Alireza-t7@users.noreply.github.com>
2026-04-09 10:05:11 +0200

    server.js aktualisieren

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(__dirname));
+
+function getUsers() {
+  const data = fs.readFileSync(path.join(__dirname, "users.json"), "utf-8");
+  return JSON.parse(data);
+}
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.post("/login", function (req, res) {
   const { identifier, password } = req.body;
 
-  console.log("Identifier:", identifier);
-  console.log("Password:", password);
+  const users = getUsers();
 
-  res.send("Login route working");
+  const user = users.find(
+    (u) => u.username === identifier || u.email === identifier
+  );
+
+  if (!user) {
+    return res.send("User not found");
+  }
+
+  if (user.password !== password) {
+    return res.send("Wrong password");
+  }
+
+  return res.send("You are logged in");
 });
 
 const PORT = process.env.PORT || 3000;
